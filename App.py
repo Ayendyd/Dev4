5e4ef8f6a81f551e75e683ff8dd62e97e54cdd52
Salim <s1171796@student.windesheim.nl>
2022-05-18 12:04:24 +0200

    Test

--- App.py
+++ App.py
@@ -3,6 +3,11 @@ from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 
+@app.route("/")
+def hello_world():
+    return 'Bekijk hier de <a href="./frontend/index.html"> admins!</a> '
+
+
 @app.route("/Admin")
 def Admin():
     return "<li> Salim Azouaoui</li> <li> Sahil Qari</li> "
