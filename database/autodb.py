71f8acea4e3d43bc57f90dac4c9aeb449f62a899
Salim <s1171796@student.windesheim.nl>
2022-05-18 13:42:03 +0200

    Grote dingen

---
+++ database/autodb.py
@@ -0,0 +1,12 @@
+from flask import Flask, jsonify  # , request
+import sqlite3 as sql
+
+
+conn = sql.connect('./database/Auto.db')
+conn.row_factory = sql.Row
+
+rows = conn.execute("select * from students").fetchall()
+
+[dict(row) for row in rows]
+
+conn.close()
